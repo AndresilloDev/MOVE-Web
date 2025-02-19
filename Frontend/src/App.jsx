af2110dafd3004cc6a916e4408d3536d57d1cb03
Cesar <20233tn076@utez.edu.mx>
2025-02-18 21:07:42 -0600

    Se ha hecho la base para el componente SensorCard como ejemplo de twcss

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
+import SensorCard from './components/SensorCard'
 
 function App() {
 
   return (
     <>
-		<h1 class="text-3xl font-bold underline">
-			Hello world!
-		</h1>
+      <h1 className="text-3xl font-bold underline">
+        esta noche oscura te tortura la locura procura estar a mi altura aunque baja es tu estatura
+      </h1>
+      <SensorCard/>
     </>
 )}
 
