66cc6b14621f62702711b804d47054dcdc4956e5
Sebastian Jimenez <20233tn097@utez.edu.mx>
2025-03-05 19:14:41 -0600

    feat: Añadi modales de editar, eliminar y agregar administrador en UsersPage; modifique colores y quite ID de la tabla de usuarios

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UsersPage from "./pages/UsersPage.jsx";
 import Sidebar from "./components/Sidebar";
 import ChangePasswordRecoverPage from "./pages/ChangePasswordRecoverPage.jsx";
-import ClassroomsPage from "./pages/ClassroomsPage"; 
+import ClassroomsPage from "./pages/ClassroomsPage";
 import NotificationsPage from "./pages/NotificationsPage";
 import HomePage from "./pages/HomePage.jsx";
 import './App.css';
@@ -15,10 +15,11 @@ function App() {
             <div>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
+                    <Route path="/users" element={<UsersPage />} />
                     <Route path="/recover-password" element={<ChangePasswordRecoverPage />} />
-                    <Route path="/classrooms" element={<ClassroomsPage />} /> 
-                    <Route path="/notifications" element={<NotificationsPage />} /> 
-                    <Route path="/sidebar" element={<Sidebar />} /> 
+                    <Route path="/classrooms" element={<ClassroomsPage />} />
+                    <Route path="/notifications" element={<NotificationsPage />} />
+                    <Route path="/sidebar" element={<Sidebar />} />
                     <Route path="/login" element={<LoginPage/>} />
                 </Routes>
             </div>
--- Frontend/src/pages/UsersPage.jsx
+++ Frontend/src/pages/UsersPage.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
-import { FaHome, FaChevronRight, FaSearch, FaEdit, FaTrash, FaPlus } from "react-icons/fa";
+import { FaHome, FaChevronRight, FaSearch, FaEdit, FaTrash, FaPlus, FaTimes } from "react-icons/fa";
 
 const UsersPage = () => {
     const [searchQuery, setSearchQuery] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
-    const usersPerPage = 5;
+    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+    const [selectedUser , setSelectedUser ] = useState(null);
 
+    const usersPerPage = 5;
     const users = [
         { id: 129340, username: "20233tn097@utez", name: "Sebastian", surname: "Jimenez" },
         { id: 129341, username: "20233tn098@utez", name: "Carlos", surname: "Hernandez" },
@@ -19,29 +23,52 @@ const UsersPage = () => {
         user.username.toLowerCase().includes(searchQuery.toLowerCase())
     );
 
-    const indexOfLastUser       = currentPage * usersPerPage;
-    const indexOfFirstUser       = indexOfLastUser       - usersPerPage;
-    const currentUsers = filteredUsers.slice(indexOfFirstUser      , indexOfLastUser      );
-
+    const indexOfLastUser  = currentPage * usersPerPage;
+    const indexOfFirstUser  = indexOfLastUser  - usersPerPage;
+    const currentUsers = filteredUsers.slice(indexOfFirstUser , indexOfLastUser );
     const totalPages = Math.ceil(filteredUsers.length / usersPerPage);
 
     const handleNextPage = () => {
-        if (currentPage < totalPages) {
-            setCurrentPage(currentPage + 1);
-        }
+        if (currentPage < totalPages) setCurrentPage(currentPage + 1);
     };
 
     const handlePrevPage = () => {
-        if (currentPage > 1) {
-            setCurrentPage(currentPage - 1);
-        }
+        if (currentPage > 1) setCurrentPage(currentPage - 1);
+    };
+
+    const openEditModal = (user) => {
+        setSelectedUser (user);
+        setIsEditModalOpen(true);
+    };
+
+    const closeEditModal = () => {
+        setIsEditModalOpen(false);
+        setSelectedUser (null);
+    };
+
+    const openDeleteModal = (user) => {
+        setSelectedUser (user);
+        setIsDeleteModalOpen(true);
+    };
+
+    const closeDeleteModal = () => {
+        setIsDeleteModalOpen(false);
+        setSelectedUser (null);
+    };
+
+    const openAddModal = () => {
+        setIsAddModalOpen(true);
+    };
+
+    const closeAddModal = () => {
+        setIsAddModalOpen(false);
     };
 
     return (
-        <div className="relative min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('/bg.png')" }}>
+        <div className="relative min-h-screen bg-cover bg-center" style={{backgroundImage: "url('/bg.png')"}}>
             <div className="m-4 ml-6 flex items-center">
-                <FaHome className="mr-2" size={25} />
-                <FaChevronRight className="mr-2" size={25} />
+                <FaHome className="mr-2" size={25}/>
+                <FaChevronRight className="mr-2" size={25}/>
                 <span className="text-xl">Administradores</span>
             </div>
 
@@ -54,36 +81,35 @@ const UsersPage = () => {
                         value={searchQuery}
                         onChange={(e) => setSearchQuery(e.target.value)}
                     />
-                    <button className="bg-yellow-300 w-10 h-full flex items-center justify-center rounded-r-xl">
-                        <FaSearch size={20} color="black" />
+                    <button className="bg-[#DEFF35] w-10 h-full flex items-center justify-center rounded-r-xl">
+                        <FaSearch size={20} color="black"/>
                     </button>
                 </div>
-                <button className="bg-yellow-300 w-12 h-12 rounded-full flex items-center justify-center shadow-md">
-                    <FaPlus size={30} color="#000" />
+                <button onClick={openAddModal}
+                        className="bg-[#DEFF35] h-12 rounded-lg flex items-center justify-center shadow-md px-4">
+                    <span className="text-lg">Agregar administrador</span>
                 </button>
             </div>
 
-            <div className="flex justify-start px-4 mb-2 mt-2">
-                <span className="font-bold text-lg w-1/5 text-left">ID</span>
-                <span className="font-bold text-lg w-1/5 text-left">Usuario</span>
-                <span className="font-bold text-lg w-1/5 text-left">Nombre</span>
-                <span className="font-bold text-lg w-1/5 text-left">Apellido</span>
-                <span className="font-bold text-lg w-1/5 text-left">Acciones</span>
+            <div className="flex justify-between px-4 mb-2 mt-2">
+                <span className="font-bold text-lg w-1/5 text-center">Usuario</span>
+                <span className="font-bold text-lg w-1/5 text-center">Nombre</span>
+                <span className="font-bold text-lg w-1/5 text-center">Apellido</span>
+                <span className="font-bold text-lg w-1/5 text-center">Acciones</span>
             </div>
 
             <ul className="mt-4">
                 {currentUsers.map((user) => (
                     <li key={user.id} className="p-2 border-b border-gray-300 flex justify-between items-center">
-                        <span className="text-xl w-1/5 text-left">{user.id}</span>
-                        <span className="text-xl w-1/5 text-left">{user.username}</span>
-                        <span className="text-xl w-1/5 text-left">{user.name}</span>
-                        <span className="text-xl w-1/5 text-left">{user.surname}</span>
-                        <div className="w-1/5 flex justify-start"> {/* Cambiado a justify-start */}
-                            <button className="ml-2 text-green-500">
-                                <FaEdit size={24} />
+                        <span className="text-xl w-1/5 text-center">{user.username}</span>
+                        <span className="text-xl w-1/5 text-center">{user.name}</span>
+                        <span className="text-xl w-1/5 text-center">{user.surname}</span>
+                        <div className="w-1/5 flex justify-center">
+                            <button className="ml-2 text-green-500" onClick={() => openEditModal(user)}>
+                                <FaEdit size={24}/>
                             </button>
-                            <button className="ml-2 text-red-500">
-                                <FaTrash size={24} />
+                            <button className="ml-2 text-red-500" onClick={() => openDeleteModal(user)}>
+                                <FaTrash size={24}/>
                             </button>
                         </div>
                     </li>
@@ -91,22 +117,125 @@ const UsersPage = () => {
             </ul>
 
             <div className="flex justify-end mt-4 pr-4">
-                <button onClick={handlePrevPage} disabled={currentPage === 1} className="mr-2 bg-yellow-300 px-4 py-2 rounded">
+                <button onClick={handlePrevPage} disabled={currentPage === 1}
+                        className="mr-2 bg-[#DEFF35] px-4 py-2 rounded">
                     Anterior
                 </button>
-                {Array.from({ length: totalPages }, (_, index) => (
+                {Array.from({length: totalPages}, (_, index) => (
                     <button
                         key={index + 1}
                         onClick={() => setCurrentPage(index + 1)}
-                        className={`mx-1 px-3 py-1 rounded ${currentPage === index + 1 ? 'bg-yellow-300' : 'bg-gray-200'}`}
+                        className={`mx-1 px-3 py-1 rounded ${currentPage === index + 1 ? 'bg-[#DEFF35]' : 'bg-gray-200'}`}
                     >
                         {index + 1}
                     </button>
                 ))}
-                <button onClick={handleNextPage} disabled={currentPage === totalPages} className="bg-yellow-300 px-4 py-2 rounded">
+                <button onClick={handleNextPage} disabled={currentPage === totalPages}
+                        className="bg-[#DEFF35] px-4 py-2 rounded">
                     Siguiente
                 </button>
             </div>
+
+            {isEditModalOpen && (
+                <div className="fixed inset-0 flex items-center justify-center backdrop-blur-md bg-white/10">
+                    <div className="bg-white p-6 rounded-2xl shadow-lg w-96 relative border border-black">
+                        <button onClick={closeEditModal} className="absolute top-2 right-2 text-black">
+                            <FaTimes size={20}/>
+                        </button>
+                        <h2 className="text-xl font-bold mb-4 text-center">Editar Administrador</h2>
+                        <input
+                            type="text"
+                            className="w-full border border-black rounded-lg p-2 mb-3"
+                            placeholder="Nombre"
+                            defaultValue={selectedUser?.name}
+                        />
+                        <input
+                            type="text"
+                            className="w-full border border-black rounded-lg p-2 mb-3"
+                            placeholder="Apellidos"
+                            defaultValue={selectedUser?.surname}
+                        />
+                        <input
+                            type="email"
+                            className="w-full border border-black rounded-lg p-2 mb-3"
+                            placeholder="Correo"
+                            defaultValue={selectedUser?.username}
+                        />
+                        <div className="flex justify-center space-x-4 mt-4">
+                            <button onClick={closeEditModal} className="border border-black px-4 py-2 rounded-lg">
+                                Cancelar
+                            </button>
+                            <button className="border border-black bg-[#DEFF35] px-4 py-2 rounded-lg">
+                                Actualizar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {isDeleteModalOpen && (
+                <div className="fixed inset-0 flex items-center justify-center backdrop-blur-md bg-white/10">
+                    <div className="bg-white p-6 rounded-2xl shadow-lg w-96 relative border border-black">
+                        <button onClick={closeDeleteModal} className="absolute top-2 right-2 text-black">
+                            <FaTimes size={20}/>
+                        </button>
+                        <h2 className="text-xl font-bold mb-4 text-center">Eliminar Administrador</h2>
+                        <p className="text-center">¿Estás seguro que deseas eliminar al administrador?</p>
+                        <div className="flex justify-center space-x-4 mt-4">
+                            <button onClick={closeDeleteModal} className="border border-black px-4 py-2 rounded-lg">
+                                Cancelar
+                            </button>
+                            <button className="border border-black bg-[#DEFF35] text-black px-4 py-2 rounded-lg">
+                                Eliminar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {isAddModalOpen && (
+                <div className="fixed inset-0 flex items-center justify-center backdrop-blur-md bg-white/10">
+                    <div className="bg-white p-6 rounded-2xl shadow-lg w-96 relative border border-black">
+                        <button onClick={closeAddModal} className="absolute top-2 right-2 text-black">
+                            <FaTimes size={20}/>
+                        </button>
+                        <h2 className="text-xl font-bold mb-4 text-center">Agregar Administrador</h2>
+                        <input
+                            type="text"
+                            className="w-full border border-black rounded-lg p-2 mb-3"
+                            placeholder="Nombre"
+                        />
+                        <input
+                            type="text"
+                            className="w-full border border-black rounded-lg p-2 mb-3"
+                            placeholder="Apellidos"
+                        />
+                        <input
+                            type="email"
+                            className="w-full border border-black rounded-lg p-2 mb-3"
+                            placeholder="Correo"
+                        />
+                        <input
+                            type="password"
+                            className="w-full border border-black rounded-lg p-2 mb-3"
+                            placeholder="Contraseña"
+                        />
+                        <input
+                            type="password"
+                            className="w-full border border-black rounded-lg p-2 mb-3"
+                            placeholder="Confirmar Contraseña"
+                        />
+                        <div className="flex justify-center space-x-4 mt-4">
+                            <button onClick={closeAddModal} className="border border-black px-4 py-2 rounded-lg">
+                                Cancelar
+                            </button>
+                            <button className="border border-black bg-[#DEFF35] px-4 py-2 rounded-lg">
+                                Agregar
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
