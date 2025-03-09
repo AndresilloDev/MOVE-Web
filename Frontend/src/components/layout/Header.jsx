a16fd4ce7f9344c415dd4fb79bbabb8266ff3ede
Andres Garcia <20233tn107@utez.edu.mx>
2025-03-09 17:39:58 -0600

    fix: Correciones menores de responsividad

--- Frontend/src/components/layout/Header.jsx
+++ Frontend/src/components/layout/Header.jsx
@@ -6,13 +6,20 @@ const Header = ({ isLoggedIn }) => {
     const navigate = useNavigate();
 
     return (
-        <div className="bg-header shadow-md w-full h-12 flex items-center justify-between p-10 px-22">
-            <div className="logo">
-                <img src="../../src/assets/logo.png" alt="MOVE" className="h-10 w-auto"/>
+        <div className="bg-header shadow-md w-full h-12 lg:h-16 flex items-center justify-between p-4 sm:p-6 lg:px-22">
+            <div className="logo cursor-pointer" onClick={() => navigate("/")}>
+                <img src="../../src/assets/logo.png" alt="MOVE" className="h-8 sm:h-10 w-auto"/>
             </div>
-            <div className="auth">  
+
+            <div className="auth">
                 {!isLoggedIn && (
-                    <ButtonBox text="Iniciar Sesión" to="/login" width="150px" height="40px" borderRadius="lg" />
+                    <ButtonBox 
+                        text="Iniciar Sesión" 
+                        to="/login" 
+                        height="36px" 
+                        smHeight="40px"
+                        borderRadius="lg" 
+                    />
                 )}
             </div>
         </div>
--- Frontend/src/pages/HomePage/index.jsx
+++ Frontend/src/pages/HomePage/index.jsx
@@ -1,14 +1,25 @@
 export default function HomePage() {
-    
-    return(
-        <div className="flex flex-row p-16">
-            <div className="w-5/12 flex flex-col justify-center pl-10 pr-4">
-                <h1 className="font-['UNCAGE-Bold'] text-5xl font-bold text-left mb-6">MONITOR DE VARIABLES DE ENTORNO</h1>
-                <p className="font-['Helvetica-Regular'] text-lg font-normal text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptas placeat numquam te.</p>
-            </div>
-            <div className="w-7/12 flex items-center justify-center p-10">
-                <img src="../../src/assets/dashboard.png" alt="Monitor" className="shadow-2xl rounded-2xl" />
-            </div>
+    return (
+      <div className="flex flex-col lg:flex-row items-center p-6 lg:p-16">
+        {/* Sección de Texto */}
+        <div className="w-full lg:w-5/12 flex flex-col justify-center px-4 lg:pl-10 lg:pr-4 text-center lg:text-left">
+          <h1 className="font-[UNCAGE-Bold] text-4xl sm:text-4xl lg:text-5xl font-bold mb-12 mt-8">
+            MONITOR DE VARIABLES DE ENTORNO
+          </h1>
+          <p className="font-helvetica text-base sm:text-lg font-normal mb-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur voluptas placeat numquam te.
+          </p>
         </div>
-    )
-}
+  
+        {/* Sección de Imagen */}
+        <div className="w-full lg:w-7/12 flex items-center justify-center p-6">
+          <img
+            src="../../src/assets/dashboard.png"
+            alt="Monitor"
+            className="max-w-full h-auto shadow-2xl rounded-2xl"
+          />
+        </div>
+      </div>
+    );
+  }
+  
