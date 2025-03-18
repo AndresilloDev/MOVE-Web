3943eebfad4814d286fc7fb9ee95823d3e78be5e
Andres Garcia <20233tn107@utez.edu.mx>
2025-03-17 20:17:46 -0600

    fix: Arregle las rutas en devices.api

--- src/api/devices.api.js
+++ src/api/devices.api.js
@@ -12,44 +12,32 @@ export const getDevices = async () => {
     }
 };
 
-
 export const getDevice = async (deviceId) => {
     try {
-        const response = await api.get(`/${endpoint}/${deviceId}`);
+        const response = await api.get(`${endpoint}/${deviceId}`);
         return response.data;
     } catch (error) {
         console.error("getDevice error:", error);
         return {};
     }
-}
-
-//Nota: Lógica por implementar (Conexión por Bluetooth)
-export const connectDevice = async (deviceId) => {
-    try {
-        const response = await api.get(`/${endpoint}/${deviceId}/connect`);
-        return response.data;
-    } catch (error) {
-        console.error("connectDevice error:", error);
-        return {};
-    }
-}
+};
 
 export const updateDevice = async (deviceId, device) => {
     try {
-        const response = await api.put(`/${endpoint}/${deviceId}`, device);
+        const response = await api.put(`${endpoint}/${deviceId}`, device);
         return response.data;
     } catch (error) {
         console.error("updateDevice error:", error);
         return {};
     }
-}
+};
 
 export const deleteDevice = async (deviceId) => {
     try {
-        const response = await api.delete(`/${endpoint}/${deviceId}`);
+        const response = await api.delete(`${endpoint}/${deviceId}`);
         return response.data;
     } catch (error) {
         console.error("deleteDevice error:", error);
         return {};
     }
-}
+};
